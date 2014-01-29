6b93cfc13dd03085fc5a8dc021cf4045ad3cb998
Shotaro Murai <show.murai@gmail.com>
2014-01-29 22:31:37 +0900

    Add favicon.

--- test.js
+++ test.js
@@ -1,4 +1,16 @@
-var koshin = function(){
+// 日付、時間を表示する(同期しないと更新されない)
+var time = function(){
   var hiduke=document.lastModified;
   document.write(hiduke);
+};
+
+
+
+var convert_for_image = function(){
+
+};
+
+var change_tst_color = function(newColor) {
+  elem = document.getElementById("img");
+  elem.style.color = newColor;
 };
